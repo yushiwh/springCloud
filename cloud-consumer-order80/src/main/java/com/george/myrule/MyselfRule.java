4b452ee4743069c038466835370987fd13edcb8b
nick <94149094@qq.com>
2020-11-30 14:55:41 +0800

    修改ribbon的轮询规则

--- cloud-consumer-order80/src/main/java/com/george/myrule/MyselfRule.java
+++ cloud-consumer-order80/src/main/java/com/george/myrule/MyselfRule.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 /**
  * @author Yang Hao
  * @description Ribbon自定义IRule规则
+ * 要跳出跟@SpringBootApplication不再一个包或者子包下面，因为重新外层建包
  * @date 2020-09-16 14:24
  */
 @Configuration
@@ -15,6 +16,7 @@ public class MyselfRule {
 
     @Bean
     public IRule myRule() {
+        //随机的规则
         return new RandomRule();
     }
 }
